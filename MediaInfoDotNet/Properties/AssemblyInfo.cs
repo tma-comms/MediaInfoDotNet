541e0ce36b8d115462e889650e7dc366dffd5d8a
Andrei Papay <andrei.papay@tmacomms.com>
2016-09-08 19:18:35 +1000

    inc assemby version

--- MediaInfoDotNet/Properties/AssemblyInfo.cs
+++ MediaInfoDotNet/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediaInfoDotNet")]
+[assembly: AssemblyTitle("MediaInfoDotNet (TMAC)")]
 [assembly: AssemblyDescription("MediaInfo.NET - A fast, easy-to-use .NET wrapper for MediaInfo.dll")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("vahanus.net")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.81.40926")]
+[assembly: AssemblyVersion("0.7.81.*")]
 //[assembly: AssemblyFileVersion("0.7.*")]
